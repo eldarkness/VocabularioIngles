bff52a184ea7b8990d84033d489a5bc9992a7340
eldarkness <el_arturo_16@hotmail.com>
2022-09-23 16:35:32 +0200

    Cambiado el orden de las palabras ahora la palabra a traducir es en español y aparecera arriba y abajo la traduccion que debe poner el usuario en ingles.

--- app/src/main/java/com/eldarkness/vocabularioingles/MainActivity.java
+++ app/src/main/java/com/eldarkness/vocabularioingles/MainActivity.java
@@ -2,6 +2,7 @@ package com.eldarkness.vocabularioingles;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Application;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -14,16 +15,16 @@ public class MainActivity extends AppCompatActivity {
     public ArrayList<String> listaIng;
     public ArrayList<String> listaEsp;
     int indice;
-    TextView textoPalabraIngles;
-    EditText textoPalabraEspanol;
+    EditText textoPalabraIngles;
+    TextView textoPalabraEspanol;
     TextView textoCuadroAcierto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textoPalabraIngles = (TextView) findViewById(R.id.PalabraIngles);
-        textoPalabraEspanol = (EditText) findViewById(R.id.PalabraEspanol);
+        textoPalabraEspanol = (TextView) findViewById(R.id.PalabraEspanol);
+        textoPalabraIngles = (EditText) findViewById(R.id.PalabraIngles);
         textoCuadroAcierto = (TextView) findViewById(R.id.mensajeAcierto);
 
         listaIng = new ArrayList<>();
@@ -39,14 +40,15 @@ public class MainActivity extends AppCompatActivity {
         listaEsp.add("Hablar");
         listaIng.add("Play");
         listaEsp.add("Jugar");
-
-
     }
 
     public void PasarPalabra(View view) {
-        indice = (int) (Math.random() * listaIng.size());
+        indice = (int) (Math.random() * listaEsp.size());
         textoPalabraEspanol.setText("");
-        textoPalabraIngles.setText("" + listaIng.get(indice));
+        textoPalabraIngles.setText("");
+        textoCuadroAcierto.setText("");
+        textoPalabraEspanol.setText("" + listaEsp.get(indice));
+
 
     }
 
@@ -54,17 +56,23 @@ public class MainActivity extends AppCompatActivity {
         String palabraEspanol = textoPalabraEspanol.getText().toString();
         String palabraIngles = textoPalabraIngles.getText().toString();
 
-        if (palabraIngles.equalsIgnoreCase("")) {
+        if (palabraEspanol.equalsIgnoreCase("")) {
             textoCuadroAcierto.setText("Debes pulsar el boton Siguiente Palabra para jugar");
-        } else if (palabraEspanol.equalsIgnoreCase("")){
-             textoCuadroAcierto.setText("Debes introducir una palabra en español para poder comprobarla");
+            return;
+        } else if (palabraIngles.equalsIgnoreCase("")){
+             textoCuadroAcierto.setText("Debes introducir una palabra en Ingles para poder comprobarla");
             return;
         }
-        if(listaEsp.get(indice).equalsIgnoreCase(palabraEspanol)){
+
+        if(listaIng.get(indice).equalsIgnoreCase(palabraIngles)){
             textoCuadroAcierto.setText("¡Has acertado!");
         }else{
-            textoCuadroAcierto.setText("¡Has fallado!, la respuesta correcta era: " + listaEsp.get(indice));
+            textoCuadroAcierto.setText("¡Has fallado!, la respuesta correcta era: " + listaIng.get(indice));
         }
 
+    }
+
+    public void Salir(View view){
+        System.exit(0);
     }
 }
