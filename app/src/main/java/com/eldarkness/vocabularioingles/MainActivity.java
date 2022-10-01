3ca6fe23687bef2dd2c1e744d179d6863a2ad925
eldarkness <eldarkness86@gmail.com>
2022-10-01 17:49:54 +0200

    Se ha conseguido acceder al excel y leer las palabras una a una. Se empieza con la implementacion de la base de datos SQLITE

--- app/src/main/java/com/eldarkness/vocabularioingles/MainActivity.java
+++ app/src/main/java/com/eldarkness/vocabularioingles/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Application;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.eldarkness.vocabularioingles.BBDD.BBDD_Controller;
+import com.eldarkness.vocabularioingles.BBDD.Estructura_BBDD;
 
 import java.util.ArrayList;
 
@@ -16,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
     public ArrayList<String> listaIng;
     public ArrayList<String> listaEsp;
+    BBDD_Controller bbdd_controller;
     int indice;
     EditText textoPalabraIngles;
     TextView textoPalabraEspanol;
@@ -31,10 +37,12 @@ public class MainActivity extends AppCompatActivity {
         textoPalabraIngles = (EditText) findViewById(R.id.PalabraIngles);
         textoCuadroAcierto = (TextView) findViewById(R.id.mensajeAcierto);
         checkAcierto = (ImageView) findViewById(R.id.checkAcierto);
+        bbdd_controller = new BBDD_Controller(this);
+
 
         listaIng = new ArrayList<>();
         listaEsp = new ArrayList<>();
-
+        /*
         listaIng.add("Blue");
         listaEsp.add("Azul");
         listaIng.add("Orange");
@@ -44,9 +52,11 @@ public class MainActivity extends AppCompatActivity {
         listaIng.add("Speak");
         listaEsp.add("Hablar");
         listaIng.add("Play");
-        listaEsp.add("Jugar");
+        listaEsp.add("Jugar");*/
 
-        PasarPalabra(new View(this));
+        anadirPalabras(listaIng, listaEsp, bbdd_controller );
+
+        //PasarPalabra(new View(this));
         siguientePalabra = false;
     }
 
@@ -61,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void ComprobarPalabra(View view){
-
+        System.out.println("comprobar palabra " +listaIng.size());
         if (siguientePalabra){
             PasarPalabra(view);
             siguientePalabra = false;
@@ -92,6 +102,42 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
+
+    }
+
+    public void anadirPalabras(ArrayList<String> listaIng, ArrayList<String> listaEsp,  BBDD_Controller bbdd_controller){
+        SQLiteDatabase sqLiteDatabase = bbdd_controller.getReadableDatabase();
+
+        Cursor c = sqLiteDatabase.rawQuery("SELECT * FROM " + Estructura_BBDD.TABLE_NAME, null);
+        c.moveToFirst();
+        System.out.println(c.getCount());
+        while(!c.isAfterLast()){
+            if(!comprobarPalabraLista(listaIng, c.getString(1))){
+                listaIng.add(c.getString(1));
+                listaEsp.add(c.getString(2));
+            }
+            System.out.println(c.getString(1));
+            c.moveToNext();
+        }
+        System.out.println(listaIng.size());
+
+
+        c.close();
+
+    }
+
+    public Boolean comprobarPalabraLista(ArrayList<String> listaIng, String palabra){
+
+        for (int i = 0; i < listaIng.size(); i++){
+            System.out.println("Comprobando la palabra de la base de datos: " + palabra +
+                    "si coincide con alguna de la lista " + listaIng.get(i));
+
+            if(palabra.equalsIgnoreCase(listaIng.get(i))){
+                return true;
+            }
+
+        }
+        return false;
 
     }
 
